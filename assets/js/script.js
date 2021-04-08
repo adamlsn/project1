38dc622e77ea9f5996165e0da218037177e1a32e
Adam Olson <adamlsn@gmail.com>
2021-04-08 18:44:21 -0500

    adding spotify keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+// CLIENT ID: '10516420d1604321857930e912172451';
+// SECRET: '7d4e3de30d5a4927ae3c109786767a8e';
+
+
 // JAVASCRIPT FUNCTIONALITY OF DROPDOWN
 
 let dropdown = document.querySelector('.dropdown');
             dropdown.addEventListener('click', function(event) {
                 event.stopPropagation();
                 dropdown.classList.toggle('is-active');
-            });
+            })
