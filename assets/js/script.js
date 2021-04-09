44744f5e8105922b1c906363b17111ed5b4a87a4
Adam Olson <adamlsn@gmail.com>
2021-04-09 15:36:37 -0500

    adding functionality to randomly select a drink name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,47 @@
 // CLIENT ID: '10516420d1604321857930e912172451';
 // SECRET: '7d4e3de30d5a4927ae3c109786767a8e';
 
-//GENERATE COCKTAIL FUNCTION
+//***************************
+//COCKTAIL DB API
+//***************************
 let sprit = "";
 
 //BUTTON CLICK LISTENERS
 $("#bourbon").click(function(){
   sprit = "Bourbon";
-  console.log(sprit);
+  console.log("CHOSEN SPRIT IS: " + sprit);
   generateCoctail();
 });
 
 $("#rum").click(function(){
   sprit = "Rum";
-  console.log(sprit);
+  console.log("CHOSEN SPRIT IS: " + sprit);
   generateCoctail();
 });
 
 $("#vodka").click(function(){
   sprit = "Vodka";
-  console.log(sprit);
+  console.log("CHOSEN SPRIT IS: " + sprit);
   generateCoctail();
 });
 
 $("#gin").click(function(){
   sprit = "Gin";
-  console.log(sprit);
+  console.log("CHOSEN SPRIT IS: " + sprit);
   generateCoctail();
 });
 
 $("#tequila").click(function(){
   sprit = "Tequila";
-  console.log(sprit);
+  console.log("CHOSEN SPRIT IS: " + sprit);
   generateCoctail();
 });
 
-
+//GENERATE COCKTAIL FUNCTION
 function generateCoctail(){
 	fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + sprit)
   .then(
+    //NONRESPONSE CONSOLE WARNING
     function(response) {
       if (response.status !== 200) {
         console.log('Looks like there was a problem. Status Code: ' +
@@ -46,10 +49,13 @@ function generateCoctail(){
         return;
       }
 
-      // Examine the text in the response
+      //ACTUAL BODY OF FUNCTION
       response.json().then(function(data) {
-        console.log(data);
-        
+
+        let drinkInteger = Math.floor(Math.random() * data.drinks.length);
+
+        let drinkName = data.drinks[drinkInteger].strDrink;
+        console.log("DRINK NAME: " + drinkName);
       });
     }
   )
