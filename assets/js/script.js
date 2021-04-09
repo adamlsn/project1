ae738a94492d0203ccbf2bf598759bc5dfa6adbd
Adam Olson <adamlsn@gmail.com>
2021-04-09 14:45:58 -0500

    adding functional button clicks that access correct drink lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,43 @@
 // CLIENT ID: '10516420d1604321857930e912172451';
 // SECRET: '7d4e3de30d5a4927ae3c109786767a8e';
-<<<<<<< HEAD
+
+//GENERATE COCKTAIL FUNCTION
+let sprit = "";
+
+//BUTTON CLICK LISTENERS
+$("#bourbon").click(function(){
+  sprit = "Bourbon";
+  console.log(sprit);
+  generateCoctail();
+});
+
+$("#rum").click(function(){
+  sprit = "Rum";
+  console.log(sprit);
+  generateCoctail();
+});
+
+$("#vodka").click(function(){
+  sprit = "Vodka";
+  console.log(sprit);
+  generateCoctail();
+});
+
+$("#gin").click(function(){
+  sprit = "Gin";
+  console.log(sprit);
+  generateCoctail();
+});
+
+$("#tequila").click(function(){
+  sprit = "Tequila";
+  console.log(sprit);
+  generateCoctail();
+});
 
 
-// JAVASCRIPT FUNCTIONALITY OF DROPDOWN
-
-let dropdown = document.querySelector('.dropdown');
-            dropdown.addEventListener('click', function(event) {
-                event.stopPropagation();
-                dropdown.classList.toggle('is-active');
-            })
-
-// JAVASCRIPT FUNCTIONALITY OF COCKTAIL API
-
-function getCocktailName () {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Rum')
+function generateCoctail(){
+	fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + sprit)
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -23,45 +46,14 @@ function getCocktailName () {
         return;
       }
 
+      // Examine the text in the response
       response.json().then(function(data) {
         console.log(data);
-        displayCocktailName(data);
+        
       });
     }
   )
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
-
 }
-
-getCocktailName ();
-
-function displayCocktailName(cocktail){
-    console.log(cocktail.drinks[0]);
-
-    let drinkSection = document.querySelector('#drink-section');
-
-    let drinkName = document.createElement('h2');
-    drinkName.innerHTML = cocktail.drinks[0].strDrink;
-
-    drinkSection.appendChild(drinkName);
-
-    let img = document.createElement('img');
-    img.src = cocktail.drinks[0].strDrinkThumb;
-
-    drinkSection.appendChild(img);
-
-    for(let i=1; i<16; i++){
-       console.log(i); 
-
-      let ingredient = document.createElement('ons-list-item');
-      ingredient.innerHTML = cocktail.drinks[0] ['strIngredient${i}'];
-
-      drinkSection.appendChild(ingredient);
-    }
-
-
-}
-=======
->>>>>>> feature/styling
