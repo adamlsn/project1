94bf6663843a10d64d44aabe2518c4e1be881f3a
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-04-09 10:54:57 -0500

    second attempt to build a an age modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+
+
+setTimeout(function(){
+    $(".modal").show();
+},1000);
+
+
+// Gettin age of the user
+$(".submit-btn").on("click",function(){
+    const today = moment().format("MMMM D YYYY");
+    console.log(today);
+    const ageMonth = $("#month").val();
+    const ageDay = $("#day").val();
+    const ageYear = $("#year").val();
+})
 // JAVASCRIPT FUNCTIONALITY OF DROPDOWN
 
 let dropdown = document.querySelector('.dropdown');
@@ -6,8 +21,8 @@ let dropdown = document.querySelector('.dropdown');
                 dropdown.classList.toggle('is-active');
             });
 
-// Gettin age of the user
-const today = moment().format("DD,MMMM,YYYY");
-console.log(today);
+
+
+
 
 
