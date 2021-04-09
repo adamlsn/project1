b76349fb1ae31dd6f0ea7a8ae5465f553a32b2b8
Your Name <you@example.com>
2021-04-08 21:10:49 -0500

    rough working cocktail API functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,56 @@ let dropdown = document.querySelector('.dropdown');
                 event.stopPropagation();
                 dropdown.classList.toggle('is-active');
             })
+
+// JAVASCRIPT FUNCTIONALITY OF COCKTAIL API
+
+function getCocktailName () {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Rum')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      response.json().then(function(data) {
+        console.log(data);
+        displayCocktailName(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+}
+
+getCocktailName ();
+
+function displayCocktailName(cocktail){
+    console.log(cocktail.drinks[0]);
+
+    let drinkSection = document.querySelector('#drink-section');
+
+    let drinkName = document.createElement('h2');
+    drinkName.innerHTML = cocktail.drinks[0].strDrink;
+
+    drinkSection.appendChild(drinkName);
+
+    let img = document.createElement('img');
+    img.src = cocktail.drinks[0].strDrinkThumb;
+
+    drinkSection.appendChild(img);
+
+    for(let i=1; i<16; i++){
+       console.log(i); 
+
+      let ingredient = document.createElement('ons-list-item');
+      ingredient.innerHTML = cocktail.drinks[0] ['strIngredient${i}'];
+
+      drinkSection.appendChild(ingredient);
+    }
+
+
+}
